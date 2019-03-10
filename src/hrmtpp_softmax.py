fixing hrmtpp softmax with binary marker type

src/hrmtpp_softmax.py
@@ -18,13 +18,13 @@ DEBUG = False
 def one_hot_encoding(y, n_dims=None):
     # Implement
     """ Take integer y (tensor or variable) with n dims and convert it to 1-hot representation with n+1 dims. """
-    y_tensor = y.data if isinstance(y, Variable) else y
+    y_tensor = y
     y_tensor = y_tensor.type(torch.LongTensor).view(-1, 1)
     n_dims = n_dims if n_dims is not None else int(torch.max(y_tensor)) + 1
     y_one_hot = torch.zeros(
         y_tensor.size()[0], n_dims).scatter_(1, y_tensor, 1)
     y_one_hot = y_one_hot.view(*y.shape, -1)
-    return Variable(y_one_hot) if isinstance(y, Variable) else y_one_hot
+    return y_one_hot
 
 def sample_gumbel(shape, eps=1e-20):
     U = torch.rand(shape)
@@ -95,9 +95,6 @@ class hrmtpp_softmax(nn.Module):
     def assert_input(self):
         assert self.marker_type in {
             'real', 'categorical', 'binary'}, "Unknown Input type provided!"
-        if self.marker_type == 'binary' and marker_dim != 2:
-            self.marker_dim = 2
-            print("Setting marker dimension to 2 for binary input!")
 
     def create_rnn_network(self):
         if self.use_rnn_cell:
@@ -139,7 +136,7 @@ class hrmtpp_softmax(nn.Module):
 
     def create_output_marker_layer(self):
         embed_module = nn.Sequential(
-            nn.Linear(self.hidden_dim + self.latent_dim, self.shared_output_layers[0])#, nn.ReLU(),
+            nn.Linear(self.hidden_dim + self.n_cluster, self.shared_output_layers[0])#, nn.ReLU(),
             #nn.Linear(self.shared_output_layers[0], self.shared_output_layers[1]), nn.ReLU()
         )
 
@@ -211,37 +208,33 @@ class hrmtpp_softmax(nn.Module):
 
             h = torch.cat(outs, dim=0)  # shape = [T+1, batchsize, h]
 
-            phi_flipped = torch.flip(phi, [0])
-            if mask is not None:
-                mask_flipped = torch.flip(mask, [0])
-            reverse_outs = []
+            recog_outs = []
             rh_t = torch.zeros(batch_size, self.hidden_dim).to(device)
-            reverse_outs.append(rh_t[None,:,:])
+            recog_outs.append(rh_t[None,:,:])
             for seq in range(seq_length):
-                rh_t = self.backward_rnn_cell(phi_flipped[seq, :, :], rh_t)
+                rh_t = self.backward_rnn_cell(phi[seq, :, :], rh_t)
                 if mask is None:
-                    reverse_outs.append(rh_t[None, :, :])
+                    recog_outs.append(rh_t[None, :, :])
                 else:
-                    reverse_outs.append(rh_t[None, :, :] * mask_flipped[seq,:][None,:,None])# 1xBSxhidden_dim * 1xBSx1 Broadcasting
+                    recog_outs.append(rh_t[None, :, :] * mask[seq,:][None,:,None])# 1xBSxhidden_dim * 1xBSx1 Broadcasting
 
-            rh_flipped = torch.cat(reverse_outs, dim=0)  # shape = [T+1, batchsize, h]
-            rh = torch.flip(rh_flipped, [0])
+            rh = torch.cat(recog_outs, dim=0)  # shape = [T+1, batchsize, h]
 
             #Now for generation at time i we need h_{i-1}. For encoder network at time i, we need a_i in the reverse rnn
-            return h[:-1,:,:], rh[1:,:,:]
+            return h[:-1,:,:], h[-1,:,:], rh[-1,:,:]
         else:
             # Run RNN over the concatenated sequence [marker_seq_emb, time_seq_emb]
             h_0 = torch.zeros(1, batch_size, self.hidden_dim).to(device)
-            hidden_seq, _ = self.forward_rnn_cell(phi, h_0)
-            h = torch.cat([h_0, hidden_seq], dim = 0)[:-1,:,:]
+            hidden_seq, h_last = self.forward_rnn_cell(phi, h_0)
+            h = torch.cat([h_0, hidden_seq], dim = 0)
 
-            phi_flipped = torch.flip(phi, [0])
+            flipped = torch.flip(phi, [0])
             rh_0 = torch.zeros(1,batch_size, self.hidden_dim).to(device)
-            r_hidden_seq, _ = self.backward_rnn_cell(phi_flipped, rh_0)
-            rh_flipped = torch.cat([rh_0, r_hidden_seq], dim = 0)
-            rh = torch.flip(rh_flipped, [0])[1:,:,:]
+            r_hidden_seq, rh_last = self.backward_rnn_cell(phi, rh_0)
+            rh = torch.cat([rh_0, r_hidden_seq], dim = 0)
+            rh = torch.flip(rh, [0])
 
-            return h, rh
+            return h[:-1,:,:], h_last[0,:,:], rh_last[0,:,:]
 
 
 
@@ -274,7 +267,7 @@ class hrmtpp_softmax(nn.Module):
                 phi_t : Tensor of shape TxBSx self.t_embedding_layer[-1]
                 phi   : Tensor of shape TxBS x (self.x_embedding_layer[-1] + self.t_embedding_layer[-1])
         """
-        if self.marker_type != 'real':
+        if self.marker_type == 'categorical':
             # Shape TxBSxmarker_dim
             x = one_hot_encoding(x[:, :, 0], self.marker_dim)
         phi_x = self.embed_x(x)
@@ -288,15 +281,15 @@ class hrmtpp_softmax(nn.Module):
             be used. But any of them can be a unbiased estimator. Need to add all of them with penalty that they are from
             same distribution
             input:
-                h : Tensor of shape (T)xBSxhidden_dim
-                h_b : Tensor of shape (T)xBSxhidden_dim
+                h : Tensor of shape BSxhidden_dim
+                h_b : Tensor of shape BSxhidden_dim
             output:
-                logits : TxBSxcategorical_dim
+                logits : 1xBSxcategorical_dim
                 
 
         """
         logits = self.inference_net(torch.cat([hs, back_hs], -1))
-        return logits
+        return logits[None, :, :]
 
     def reparameterize(self, mu, logvar):
         epsilon = torch.randn_like(mu)
@@ -315,11 +308,11 @@ class hrmtpp_softmax(nn.Module):
 
         """
 
-        # Tensor of shape (T)xBSxhidden_dim
-        hs, back_hs = self.run_forward_backward_rnn(x, t, mask)
+        # hs: Tensor of shape (T)xBSxhidden_dim, h_last: BSxhidden_dim
+        hs, h_last, rh_last = self.run_forward_backward_rnn(x, t, mask)
 
-        logits = self.encoder(hs, back_hs) #TxBSxcategorical_dim
-        z = gumbel_softmax(logits, temp)#TxBSxcategorical_dim
+        logits = self.encoder(h_last, rh_last) #1xBSxcategorical_dim
+        z = gumbel_softmax(logits, temp)#1xBSxcategorical_dim
 
         hz_embedded = self.preprocess_hidden_latent_state(hs, z)
         time_log_likelihood = self.compute_point_log_likelihood(hz_embedded,  t)
@@ -330,10 +323,11 @@ class hrmtpp_softmax(nn.Module):
 
 
         #KL divergence Loss
-        log_ratio = torch.log(logits * self.n_cluster + 1e-20)#TxBSxC
-        KLD = torch.sum(logits * log_ratio, dim=-1)#TxBS
+        prob_ = F.softmax(logits, dim =-1)
+        log_ratio = torch.log(prob_ * self.n_cluster + 1e-20)#TxBSxC
+        KLD = torch.sum(prob_ * log_ratio, dim=-1)#TxBS
 
-        return time_log_likelihood, marker_log_likelihood, kld_loss  # TxBS and TxBS
+        return time_log_likelihood, marker_log_likelihood, KLD  # TxBS and TxBS
 
     def compute_marker_log_likelihood(self, x, mu, logvar):
         """
@@ -353,10 +347,13 @@ class hrmtpp_softmax(nn.Module):
             return ll_loss
         else:
             seq_lengths, batch_size = x.size(0), x.size(1)
-            mu_ = mu.view(-1, self.marker_dim)  # T*BS x marker_dim
-            x_ = x.view(-1)  # (T*BS,)
-            loss = F.cross_entropy(mu_, x_, reduction='none').view(
-                seq_lengths, batch_size)
+            if self.marker_type == 'categorical':
+                mu_ = mu.view(-1, self.marker_dim)  # T*BS x marker_dim
+                x_ = x.view(-1)  # (T*BS,)
+                loss = F.cross_entropy(mu_, x_, reduction='none').view(
+                    seq_lengths, batch_size)
+            else:
+                loss = F.binary_cross_entropy_with_logits(mu, x, reduction= 'none').sum(dim =-1)#TxBS
             return -loss
 
     def compute_point_log_likelihood(self, h, t):
@@ -382,7 +379,7 @@ class hrmtpp_softmax(nn.Module):
         term3 = term1.exp()
 
         log_f_t = term1 + \
-            (1./self.time_influence) * (term2-term3)
+            (1./(self.time_influence+1e-6)) * (term2-term3)
         return log_f_t[:, :, 0]  # TxBS
 
     def generate_marker(self, h, t):