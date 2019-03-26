normalizing actual time, fixing first time interval in mimic, minor pipeline changes

src/run.py
@@ -37,6 +37,7 @@ def train(net, params, optimizer, x_data, t_data, label):
     marker_mse, marker_mse_count = 0., 0.
     marker_acc, marker_acc_count = 0., 0.
     total_loss = 0.
+    total_sequence = 0.
 
     if params.show:
         from utils.helper import ProgressBar
@@ -50,6 +51,7 @@ def train(net, params, optimizer, x_data, t_data, label):
         unpad_input_t = t_data[b_idx*batch_size:(b_idx+1)*batch_size]
 
         seq_len = [len(lst) for lst in unpad_input_x]
+        total_sequence += sum(seq_len)
         max_seq_len = max(seq_len)
         # Shape = T_max_batch x BS x marker_dim
         input_x = np.zeros( (max_seq_len, len(seq_len), unpad_input_x[0].shape[1]) )
@@ -92,6 +94,7 @@ def train(net, params, optimizer, x_data, t_data, label):
     if params.show: bar.finish()
 
     time_rmse = (time_mse/time_mse_count)** 0.5
+    total_loss /= total_sequence
     if params.marker_type == 'real':
         marker_rmse = (marker_mse/marker_mse_count)** 0.5
         accuracy = None
@@ -118,6 +121,7 @@ def test(net, params,  optimizer,  x_data, t_data, label):
     marker_mse, marker_mse_count = 0., 0.
     marker_acc, marker_acc_count = 0., 0.
     total_loss = 0.
+    total_sequence =0
 
     if params.show:
         from utils.helper import ProgressBar
@@ -131,6 +135,7 @@ def test(net, params,  optimizer,  x_data, t_data, label):
         unpad_input_t = t_data[b_idx*batch_size:(b_idx+1)*batch_size]
 
         seq_len = [len(lst) for lst in unpad_input_x]
+        total_sequence += sum(seq_len)
         max_seq_len = max(seq_len)
         input_x = np.zeros( (max_seq_len, len(seq_len), unpad_input_x[0].shape[1]) )
         input_t = np.zeros( (max_seq_len, len(seq_len), unpad_input_t[0].shape[1]) )
@@ -173,5 +178,5 @@ def test(net, params,  optimizer,  x_data, t_data, label):
         accuracy = marker_acc/marker_acc_count
         auc = None
         marker_rmse = None
-
+    total_loss /= total_sequence
     return total_loss, time_rmse, accuracy, auc, marker_rmse

src/utils/mimic_processing.py
@@ -170,11 +170,24 @@ def compute_times(data):
         shifted = np.concatenate([np.zeros_like(admit_times[0:1]), admit_times[:-1]], axis=0)
         intervals = admit_times - shifted
         # Convert nanoseconds to years
+        #changing inervals at index 1 and visit duration at index 2
         t_data_i = np.stack([admit_times.values.astype(int),
-                           visit_durations.values.astype(int),
-                           intervals.values.astype(int)]).T / (1e9*3600*24*365.25)
+                           intervals.values.astype(int),
+                           visit_durations.values.astype(int)]).T / (1e9*3600*24*365.25)
         t_data.append(t_data_i)
     return t_data
+
+def fix_normalize_time(data):
+    N = len(data)
+    admit_times = np.concatenate([i[:,0] for i in data])
+    interval_times = np.concatenate([i[1:,1] for i in data])
+    mean_admit_time, std_admit_time = np.mean(admit_times), np.std(admit_times)
+    mean_interval_time, std_interval_time = np.mean(interval_times), np.std(interval_times)
+
+    for idx in range(N):
+        data[idx][:,0] = (data[idx][:,0]-mean_admit_time)/std_admit_time
+        data[idx][0,1] = mean_interval_time
+    return data
 
 def save_mimic_data(data=None):
     """
@@ -189,6 +202,7 @@ def save_mimic_data(data=None):
             data = pickle.load(handle)
 
     t_data = compute_times(data)
+    t_data = fix_normalize_time(t_data)
     x_data = compute_markers(data)
     assert(np.all([t_data[idx].shape[1] == 3 for idx in range(100)]))
     assert(np.all([x_data[idx].shape[0] == t_data[idx].shape[0] for idx in range(100)]))
@@ -197,14 +211,17 @@ def save_mimic_data(data=None):
         't': t_data,
         'x': x_data
     }
-    
-    train_dict, val_dict = train_val_split(data_dict, val_ratio=0.2)
+    #Train-Valid-Test Split of 60-20-20
+    train_dict, extra_dict = train_val_split(data_dict, val_ratio=0.4)
+    val_dict, test_dict = train_val_split(extra_dict, val_ratio=0.5)
     assert(len(train_dict['x']) == len(train_dict['t']))
     assert(len(val_dict['x']) == len(val_dict['t']))
-    assert(len(train_dict['x']) + len(val_dict['x']) == len(data_dict['x']))
+    assert(len(test_dict['x']) == len(test_dict['t']))
+    assert(len(train_dict['x']) + len(val_dict['x'])+ len(test_dict['x']) == len(data_dict['x']))
 
     train_path_data = '../data/mimic_train.pkl'
     valid_path_data = '../data/mimic_valid.pkl'
+    test_path_data = '../data/mimic_test.pkl'
 
     print("Saving training data to {}".format(train_path_data))
     with open(train_path_data, 'wb') as handle:
@@ -212,7 +229,10 @@ def save_mimic_data(data=None):
     print("Saving validation data to {}".format(valid_path_data))
     with open(valid_path_data, 'wb') as handle:
         pickle.dump(val_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    return train_path_data, valid_path_data
+    print("Saving testing data to {}".format(test_path_data))
+    with open(test_path_data, 'wb') as handle:
+        pickle.dump(test_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    return train_path_data, valid_path_data, test_path_data
 
 
 if __name__ == "__main__":
@@ -224,4 +244,4 @@ if __name__ == "__main__":
     # else:
     #     data = preprocess_raw_data()
     # print(len(data))
-    save_mimic_data()
+    print(save_mimic_data())