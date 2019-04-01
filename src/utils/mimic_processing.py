d60f733ac3321d518eebd8a1c164773a05b85f77
Aritra Ghosh <aritraghosh.iem@gmail.com>
2019-04-01 03:32:49 -0400

    mimic icd9code conversion

--- src/utils/mimic_processing.py
+++ src/utils/mimic_processing.py
@@ -6,12 +6,13 @@ import pandas
 from sklearn.preprocessing import MultiLabelBinarizer
 from helper import train_val_split
 
+
 def valid_subject(path):
-    #files = [os.path.join(path, o) for o in os.listdir(path) 
+    # files = [os.path.join(path, o) for o in os.listdir(path)
     #                if os.path.isdir(os.path.join(d,o)) is False]
     file = os.path.join(path, 'stays.csv')
     line_count = 0
-    with open(file,mode='r') as csv_file:
+    with open(file, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
 
         for row in csv_reader:
@@ -19,9 +20,10 @@ def valid_subject(path):
                 #print(f'Column names are {", ".join(row)}')
                 line_count += 1
             line_count += 1
-            #print(row["ICUSTAY_ID"])
+            # print(row["ICUSTAY_ID"])
         #print(f'Processed {line_count} lines.')
     return line_count-1
+
 
 def get_raw_data(path):
     stay_file = os.path.join(path, 'stays.csv')
@@ -29,8 +31,9 @@ def get_raw_data(path):
     line_count = 0
     patient_dic = {}
     id_to_icu_map = {}
-    fields = ["SUBJECT_ID","ICUSTAY_ID", "HADM_ID", "INTIME", "OUTTIME", "LOS", "ADMITTIME", "DISCHTIME","DEATHTIME", "ETHNICITY", "GENDER", "DOB", "AGE", "MORTALITY"]
-    with open(stay_file,mode='r') as csv_file:
+    fields = ["SUBJECT_ID", "ICUSTAY_ID", "HADM_ID", "INTIME", "OUTTIME", "LOS", "ADMITTIME",
+              "DISCHTIME", "DEATHTIME", "ETHNICITY", "GENDER", "DOB", "AGE", "MORTALITY"]
+    with open(stay_file, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             if line_count == 0:
@@ -43,8 +46,8 @@ def get_raw_data(path):
             episode_info["ICD9_CODE"] = []
             id_to_icu_map[row["ICUSTAY_ID"]] = len(patient_dic)
             patient_dic[len(patient_dic)] = episode_info
-    
-    with open(diag_file,mode='r') as csv_file:
+
+    with open(diag_file, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             if line_count == 0:
@@ -58,25 +61,26 @@ def get_raw_data(path):
 
 def preprocess_raw_data():
     d = './../data/mimic3-benchmarks/data/root/'
-    subject_path = [os.path.join(d, o) for o in os.listdir(d) 
-                        if os.path.isdir(os.path.join(d,o))]
+    subject_path = [os.path.join(d, o) for o in os.listdir(d)
+                    if os.path.isdir(os.path.join(d, o))]
     threshold = 3
-    valid_subject_list = [o for o in subject_path if valid_subject(o)>=threshold]
+    valid_subject_list = [
+        o for o in subject_path if valid_subject(o) >= threshold]
     print(len(valid_subject_list), len(subject_path))
 
     raw_data = [get_raw_data(o) for o in valid_subject_list]
-    #print(raw_data[0])
+    # print(raw_data[0])
     with open(raw_file, 'wb') as handle:
         pickle.dump(raw_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
     return raw_data
-
 
 
 #############
 
 
 def gender_to_categorical(x):
-    return 0 if x=='M' else 1
+    return 0 if x == 'M' else 1
+
 
 def mimic_correct_datatypes(icu_df):
 
@@ -99,20 +103,21 @@ def mimic_correct_datatypes(icu_df):
     icu_df['MORTALITY'] = icu_df['MORTALITY'].astype('int')
     return icu_df
 
+
 def mimic_data_to_df(data, colnames=None):
     flat_data = [icu_visit for data_i in data for icu_visit in data_i.values()]
     icu_df = pandas.DataFrame(flat_data)
     icu_df = mimic_correct_datatypes(icu_df)
     # Make gender a 0/1 variable
     icu_df['GENDER'] = icu_df['GENDER'].apply(gender_to_categorical)
-    
+
     # Make ETHNICITY a one-hot feature set, removing the first column to ensure there is no linear dependence
 #     ethnicity_dummies = pandas.get_dummies(icu_df.ETHNICITY, drop_first=True, prefix='ETHNICITY_')
 #     icu_df = pandas.concat([icu_df.drop('ETHNICITY', axis=1), ethnicity_dummies], axis=1)
-    
+
     # Center the AGE column
 #     icu_df['AGE'] = (icu_df.AGE - icu_df.AGE.mean()) / icu_df.AGE.std()
-    
+
     if colnames is None:
         return icu_df
     else:
@@ -129,7 +134,7 @@ def compute_markers(data):
     x_data = []
     icu_df = mimic_data_to_df(data)
     
-    
+
     # Restricting the icd9 codes to those
     # which have occurrence frequency of more than 80 (= 217 / 3126 codes)
     _mlb = MultiLabelBinarizer()
@@ -139,7 +144,7 @@ def compute_markers(data):
     # This WILL throw a warning (because we're skipping many codes) but that's okay
     mlb = MultiLabelBinarizer(codes_subset)
     markers_multilabel = mlb.fit_transform(icu_df.ICD9_CODE)
-    
+
     group_patients = icu_df.groupby('SUBJECT_ID')
     for patient_idx, patient_df_rows in group_patients.groups.items():
         # Get an array of shape  (T_i x marker_dim)
@@ -147,6 +152,7 @@ def compute_markers(data):
         t_i, marker_dim = patient_markers.shape
         x_data.append(patient_markers)
     return x_data
+
 
 def compute_times(data):
     """
@@ -158,14 +164,15 @@ def compute_times(data):
 #     max_visit_n = max([len(data_i) for data_i in data])
     t_data = []
     icu_df = mimic_data_to_df(data)
-    
+
     group_patients = icu_df.groupby('SUBJECT_ID')
     for patient_idx, patient_df_rows in group_patients.groups.items():
-        t_data_i = icu_df.loc[patient_df_rows][['ADMITTIME', 'INTIME', 'OUTTIME']].astype('int') / (10**9 * 3600 * 24)
+        t_data_i = icu_df.loc[patient_df_rows][[
+            'ADMITTIME', 'INTIME', 'OUTTIME']].astype('int') / (10**9 * 3600 * 24)
         admit_times = t_data_i.ADMITTIME
         visit_durations = t_data_i.OUTTIME - t_data_i.INTIME
         # import pdb; pdb.set_trace()
-        
+
         # Compute the intervals
         # Shift creates a NaT value at the first entry. Replace it with a zero in the end
         admit_intervals = admit_times - admit_times.shift(periods=1)
@@ -174,24 +181,27 @@ def compute_times(data):
         # shifted = np.concatenate([np.zeros_like(admit_times[0:1]), admit_times[:-1]], axis=0)
         # intervals = admit_times - shifted
         # Convert nanoseconds to days
-        #changing inervals at index 1 and visit duration at index 2
+        # changing inervals at index 1 and visit duration at index 2
         t_data_i = np.stack([admit_times.values,
-                           admit_intervals.values,
-                           visit_durations.values]).T
+                             admit_intervals.values,
+                             visit_durations.values]).T
         t_data.append(t_data_i)
     return t_data
 
+
 def fix_normalize_time(data):
     N = len(data)
-    admit_times = np.concatenate([i[:,0] for i in data])
-    interval_times = np.concatenate([i[1:,1] for i in data])
+    admit_times = np.concatenate([i[:, 0] for i in data])
+    interval_times = np.concatenate([i[1:, 1] for i in data])
     mean_admit_time, std_admit_time = np.mean(admit_times), np.std(admit_times)
-    mean_interval_time, std_interval_time = np.median(interval_times), np.std(interval_times)
+    mean_interval_time, std_interval_time = np.median(
+        interval_times), np.std(interval_times)
 
     for idx in range(N):
-        data[idx][:,0] = (data[idx][:,0]-mean_admit_time)/std_admit_time
-        data[idx][0,1] = mean_interval_time
+        data[idx][:, 0] = (data[idx][:, 0]-mean_admit_time)/std_admit_time
+        data[idx][0, 1] = mean_interval_time
     return data
+
 
 def save_mimic_data(data=None):
     """
@@ -200,28 +210,31 @@ def save_mimic_data(data=None):
     x_data: list of length num_data_train, each element is numpy array of shape T_i x marker_dim
     t_data: list of length num_data_train, each element is numpy array of shape T_i x 3
     """
-    if data==None:
+    if data == None:
         raw_file = './../data/dump/mimic.pickle'
         with open(raw_file, 'rb') as handle:
             data = pickle.load(handle)
 
+    x_data = compute_markers(data)
     t_data = compute_times(data)
     t_data = fix_normalize_time(t_data)
-    x_data = compute_markers(data)
+
     assert(np.all([t_data[idx].shape[1] == 3 for idx in range(100)]))
-    assert(np.all([x_data[idx].shape[0] == t_data[idx].shape[0] for idx in range(100)]))
+    assert(np.all([x_data[idx].shape[0] == t_data[idx].shape[0]
+                   for idx in range(100)]))
 
     data_dict = {
         't': t_data,
         'x': x_data
     }
-    #Train-Valid-Test Split of 60-20-20
+    # Train-Valid-Test Split of 60-20-20
     train_dict, extra_dict = train_val_split(data_dict, val_ratio=0.4)
     val_dict, test_dict = train_val_split(extra_dict, val_ratio=0.5)
     assert(len(train_dict['x']) == len(train_dict['t']))
     assert(len(val_dict['x']) == len(val_dict['t']))
     assert(len(test_dict['x']) == len(test_dict['t']))
-    assert(len(train_dict['x']) + len(val_dict['x'])+ len(test_dict['x']) == len(data_dict['x']))
+    assert(len(train_dict['x']) + len(val_dict['x']) +
+           len(test_dict['x']) == len(data_dict['x']))
 
     train_path_data = '../data/mimic_train.pkl'
     valid_path_data = '../data/mimic_valid.pkl'
@@ -248,4 +261,32 @@ if __name__ == "__main__":
     # else:
     #     data = preprocess_raw_data()
     # print(len(data))
-    print(save_mimic_data())
+    #print(save_mimic_data())
+    raw_file = './../data/dump/mimic.pickle'
+    with open(raw_file, 'rb') as handle:
+        data = pickle.load(handle)
+    values = [v['ICD9_CODE'] for data_i in data for v in data_i.values()] 
+    count = {}
+    for v in values:
+        for j in v:
+            if j not in count:
+                count[j]=1
+            else:
+                count[j] += 1
+    ls = set([k for k, v in  count.items() if v>150])
+    print(len(ls))
+
+    f_count =0
+    o_count =0
+    for v in values:
+        if v[0] in ls:
+            f_count +=1
+            continue
+        else:
+            for j in v:
+                if j in ls:
+                    o_count +=1
+                    break
+    print(f_count, o_count, len(values)- (f_count+o_count))
+
+    
