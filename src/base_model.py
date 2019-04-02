softmax fix

src/base_model.py
@@ -69,8 +69,8 @@ def create_output_marker_layer(model):
             nn.Sigmoid())
     elif model.marker_type == 'categorical':
         x_module_mu = nn.Sequential(
-            nn.Linear(l, model.marker_dim),
-            nn.Softmax(dim=-1)
+            nn.Linear(l, model.marker_dim)#,
+            #nn.Softmax(dim=-1)
         )
 
     return embed_module, x_module_mu, x_module_logvar    