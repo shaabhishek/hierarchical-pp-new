a2f1fc9008bad6cb326aaa918d1621609601d929
Abhishek Sharma <shaabhishek@gmail.com>
2019-04-06 20:59:14 -0400

    cuda bug

--- src/run.py
+++ src/run.py
@@ -83,7 +83,7 @@ def train(net, params, optimizer, x_data, t_data, label):
         loss, meta_info = net(input_x, input_t, mask=input_mask)
         loss.backward()
 
-        total_loss += meta_info['true_ll'].numpy()
+        total_loss += meta_info['true_ll'].cpu().numpy()
         
         time_mse += meta_info["time_mse"]
         time_mse_count += meta_info["time_mse_count"]
