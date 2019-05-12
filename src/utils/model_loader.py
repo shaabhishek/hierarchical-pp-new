a02f57c406486a6951c493692259c194fda110fe
Aritra Ghosh <aritraghosh.iem@gmail.com>
2019-05-11 22:07:50 -0400

    filtering, no pred

--- src/utils/model_loader.py
+++ src/utils/model_loader.py
@@ -4,6 +4,7 @@ sys.path.insert(0, './../')
 from rmtpp import rmtpp
 from autoregressive import ACD
 from model2 import Model2
+from model2_filt import Model2Filter
 from model1 import Model1
 
 def load_model(params):
@@ -15,4 +16,6 @@ def load_model(params):
         model = Model1(marker_type= params.marker_type, marker_dim = params.marker_dim, time_dim=params.time_dim, hidden_dim = params.hidden_dim,x_given_t = params.x_given_t, base_intensity = params.base_intensity, time_influence = params.time_influence, gamma = params.gamma, time_loss = params.time_loss, dropout=params.dropout)
     if params.model == 'model2':
         model = Model2(marker_type= params.marker_type, marker_dim = params.marker_dim, latent_dim=params.latent_dim, time_dim=params.time_dim, hidden_dim = params.hidden_dim, n_cluster=params.n_cluster, x_given_t = params.x_given_t, base_intensity = params.base_intensity, time_influence = params.time_influence, gamma = params.gamma, time_loss = params.time_loss, dropout=params.dropout)
+    if params.model == 'model2_filt':
+        model = Model2Filter(marker_type= params.marker_type, marker_dim = params.marker_dim, latent_dim=params.latent_dim, time_dim=params.time_dim, hidden_dim = params.hidden_dim, n_cluster=params.n_cluster, x_given_t = params.x_given_t, base_intensity = params.base_intensity, time_influence = params.time_influence, gamma = params.gamma, time_loss = params.time_loss, dropout=params.dropout)
     return model
